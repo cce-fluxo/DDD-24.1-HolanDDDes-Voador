d90e78bce4fa6cb3444e8cde687db0bda1485894
m-cf <marianafonseca@poli.ufrj.br>
2024-09-02 18:57:19 +0000

    a

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import Image from "next/image";
 import Checkin from "./checkin";
 import Checkout from "./checkout";
 import Reserva from "./reserva"
 import Quarto from "./quarto";
+import React, { useRef } from 'react';
 import Hospede from "./hospede"
+import LoggedHeader from "@/app/LoggedHeader";
 
 export default function Home() {
 
@@ -14,50 +17,52 @@ export default function Home() {
   //const hospedes = await fectch()
 
   return (
-    <div >
-        <LoggedHeader/>
+    <>
+    <div>
+      <LoggedHeader />
       <div className=" mt-10">
-        <div className="flex justify-center pt-[64px] text-red_ text-[30px]">Bem vindo ao seu hotel</div>
-        <div className=" flex justify-center pb-[64px] text-cinza text-[30px] ">Controle totalmente o seu negócio!</div>
+        <div className="flex justify-center pt-[64px] text-rosa-4 text-[30px]">Bem vindo ao seu hotel</div>
+        <div className=" flex justify-center pb-[64px] text-cinza-1 text-[30px] ">Controle totalmente o seu negócio!</div>
       </div>
       
       <div className=" ml-[42.5px]">
 
         <div className=" mb-[64px] overflow-x-auto">
           <div className=" mb-[24px]">
-            <p className=" text-[22px]">Fazendo check in (1){}</p>
+            <p className=" text-[22px] text-preto font-poppins">Fazendo check in (1){}</p>
           </div>
             <Checkin></Checkin>
         </div>
 
         <div className=" mb-[64px] overflow-x-auto">
           <div className=" mb-[24px]">
-            <p className=" text-[22px]">Fazendo check out (1){}</p>
+            <p className=" text-[22px] text-preto font-poppins">Fazendo check out (1){}</p>
           </div>
             <Checkout/>
         </div>
 
         <div className=" mb-[64px] overflow-x-auto">
           <div className=" mb-[24px]">
-            <p className=" text-[22px]">Quartos reservados nos últimos dias (1){}</p>
+            <p className=" text-[22px] text-preto font-poppins">Quartos reservados nos últimos dias (1){}</p>
           </div>
             <Reserva/>
         </div>
 
         <div className=" mb-[64px] overflow-x-auto">
           <div className=" mb-[24px]">
-            <p className=" text-[22px]">Quartos livres (1){}</p>
+            <p className=" text-[22px] text-preto font-poppins">Quartos livres (1){}</p>
           </div>
             <Quarto/>
         </div>
 
         <div className=" mb-[64px] overflow-x-auto">
           <div className=" mb-[24px]">
-            <p className=" text-[22px] font-bold">Hóspedes no momento (1){}</p>
+            <p className=" text-[22px] text-preto font-poppins">Hóspedes no momento (1){}</p>
           </div>
             <Hospede></Hospede>
         </div>
       </div>
     </div>
+    </>
   );
 }
