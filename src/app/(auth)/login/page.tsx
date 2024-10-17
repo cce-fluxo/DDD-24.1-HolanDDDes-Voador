f6166f44d12824cd9cd6e4df5237077b7c6f69bb
lucas cordeiro <lucascq.20222@poli.ufrj.br>
2024-10-17 15:29:37 -0300

    removendo o layout no cadastro

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,6 +6,7 @@ import Button from '@/app/components/button';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { useRouter } from "next/navigation";
+import AuthModal from '@/app/components/authModal';
 
 export default function auth() {
   const router = useRouter();
@@ -21,43 +22,48 @@ export default function auth() {
   };
 
   return (
-    <div className='w-full h-full flex flex-col items-center gap-[32px] mb-[10%]'>
-      <h1 className="text-2xl font-medium leading-[36px] font-poppins text-[#333333]">Faça seu login ou crie uma conta</h1>
+    <div className="bg-zinc-900 w-screen h-screen flex justify-center">
+      <AuthModal>
+        <div className='w-full h-full flex flex-col items-center gap-[32px] mb-[10%]'>
+        <h1 className="text-2xl font-medium leading-[36px] font-poppins text-[#333333]">Faça seu login ou crie uma conta</h1>
 
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        validationSchema={validationSchema}
-        onSubmit={(values) => sendForm(values)}>
-        {({ handleSubmit }) => (
-          <Form className="w-full flex flex-col items-center gap-[40px]" onSubmit={handleSubmit}>
-            <div className="w-full flex flex-col gap-[12px]">
-                <div>
-                    <Field name="email" placeholder="E-mail" component={InputField} />
-                    <ErrorMessage name="email" component="div" className="text-red-500" />
-                </div>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema={validationSchema}
+          onSubmit={(values) => sendForm(values)}>
+          {({ handleSubmit }) => (
+            <Form className="w-full flex flex-col items-center gap-[40px]" onSubmit={handleSubmit}>
+              <div className="w-full flex flex-col gap-[12px]">
+                  <div>
+                      <Field name="email" placeholder="E-mail" component={InputField} />
+                      <ErrorMessage name="email" component="div" className="text-red-500" />
+                  </div>
 
-                <div>
-                    <Field name="password" type="password" placeholder="Senha" component={InputField} />
-                    <ErrorMessage name="password" component="div" className="text-red-500" />
+                  <div>
+                      <Field name="password" type="password" placeholder="Senha" component={InputField} />
+                      <ErrorMessage name="password" component="div" className="text-red-500" />
+                  </div>
+                <div className="flex justify-between w-full">
+                  <div className="flex gap-2">
+                    <input type="checkbox" className="accent-[#589b97a1]" />
+                    <p className="text-[#372F30]">Lembrar-me</p>
+                  </div>
+                  <Link href='/redefinir-1' passHref className='text-[#372F30] underline'>
+                      Esqueceu sua senha?
+                  </Link>
                 </div>
-              <div className="flex justify-between w-full">
-                <div className="flex gap-2">
-                  <input type="checkbox" className="accent-[#589b97a1]" />
-                  <p className="text-[#372F30]">Lembrar-me</p>
-                </div>
-                <Link href='/redefinir-1' passHref className='text-[#372F30] underline'>
-                    Esqueceu sua senha?
-                </Link>
               </div>
-            </div>
 
-            <div className="w-[80%] flex flex-col  gap-[16px]">
-              <Button text="Entrar" variant="primary" type="submit" />
-              <Button href="/cadastro-1" text="Cadastre-se" variant="secundary" />
-            </div>
-          </Form>
-        )}
-      </Formik>
+              <div className="w-[80%] flex flex-col  gap-[16px]">
+                <Button text="Entrar" variant="primary" type="submit" />
+                <Button href="/cadastro-1" text="Cadastre-se" variant="secundary" />
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+      </AuthModal>
     </div>
+    
   );
 }
