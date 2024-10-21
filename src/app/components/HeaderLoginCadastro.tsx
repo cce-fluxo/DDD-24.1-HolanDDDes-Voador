6c54e25a21757f5ebe9934774c0b1bbfe3ed5248
Gustavo-Roxo <guga.amaralroxo@gmail.com>
2024-10-21 19:43:43 -0300

    headerCadastro
    melhora no header

--- src/app/components/HeaderLoginCadastro.tsx
+++ src/app/components/HeaderLoginCadastro.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
 
 const HeaderLoginCadastro = () => {
   return (
     <header className="bg-branco flex items-center justify-between px-16 font-poppins font-medium text-preto border-b border-cinza-2 h-24 z-50">
-      <div className="flex items-end gap-4">
-        <Image src="/logo.png" height={38} width={38} alt="logo" />
-        <span className="text-2xl">BonVoyage</span>
+      <div>
+        <Link href={"./"}>
+          <div className="flex items-end gap-4">
+            <Image src="/logo.png" height={38} width={38} alt="logo" />
+
+            <span className="text-2xl">BonVoyage</span>
+          </div>
+        </Link>
       </div>
       <span className="text-xl">Anuncie seu espaço com a gente</span>
     </header>
