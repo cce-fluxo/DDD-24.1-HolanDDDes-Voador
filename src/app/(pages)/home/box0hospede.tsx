1b9b1ffe08c25b7546d9ddee4a72151d3bc2e39a
coderatta <coderatta@gmail.com>
2024-11-11 20:10:32 -0300

    corrige dupliacao nos boxes das home

--- src/app/(pages)/home/box0hospede.tsx
+++ src/app/(pages)/home/box0hospede.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
-import Pessoa from "../../../../public/pessoa.png"
-
+import Pessoa from "../../../../public/pessoa.png";
 
 export default function Box0hospede() {
-    return(
-        <div className=" flex items-center min-w-[420px] min-h-[140px] gap-[24px] px-[20px] bg-cinza-2 rounded-[20px]">
-            <div className=" w-[136px] h-[100px]">
-                <Image src={Pessoa} alt=""></Image>
-            </div>
-            <div>
-                <p className=" text-[24px] font-sans text-cinza-4 font-normal font-poppins">Nenhum hóspede</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className=" flex items-center min-w-[420px] min-h-[140px] gap-[24px] px-[20px] bg-cinza-2 rounded-[20px]">
+      <div className=" w-[136px] h-[100px]">
+        <Image src={Pessoa} alt=""></Image>
+      </div>
+      <div>
+        <p className=" text-[24px] font-sans text-cinza-4 font-normal">
+          Nenhum hóspede
+        </p>
+      </div>
+    </div>
+  );
 }
