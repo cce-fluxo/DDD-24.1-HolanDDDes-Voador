e8bd6349b2b24144ac201f5922aaf02c5aba3012
FelipeCassano <162188832+cqlucasufrj@users.noreply.github.com>
2024-11-22 19:21:21 -0300

    Create layout.tsx

---
+++ src/app/(auth)/(cadastro)/layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import SingUpContextProvider from "@/app/context/signUpContext";
+import React from "react";
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+        <SingUpContextProvider>
+            {children}
+        </SingUpContextProvider>
+    </>
+  );
+};
+
+export default AuthLayout;
