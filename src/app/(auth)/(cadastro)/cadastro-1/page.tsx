03b9d609b034dca390f5e11a5d3f171e5902ec71
Breno Motta <brenormotta.20231@poli.ufrj.br>
2025-01-26 20:43:00 -0300

    Update page.tsx

--- src/app/(auth)/(cadastro)/cadastro-1/page.tsx
+++ src/app/(auth)/(cadastro)/cadastro-1/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { images } from "@/../../constants";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
